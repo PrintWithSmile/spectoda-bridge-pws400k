da85437e6a29536e6d81b3ab3049ed5dba9cb9f5
immakermatty <jsem@matejsuchanek.cz>
2023-11-24 11:27:59 +0100

    chore: do submodule update when start fails

--- pws-init-script.sh
+++ pws-init-script.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
-# Command with a timeout of 60 seconds
-timeout 60s git submodule update --init --recursive || echo "Timeout or failure in git submodule update"
+# Define the root directory of your main Git repository
+REPO_DIR="/home/pi/spectoda-bridge-pws400k"
+
+# Submodules paths
+SUBMODULES=("src/lib/node-ble" "src/lib/spectoda-js")
+
+# Navigate to the root directory
+cd $REPO_DIR
+
+timeout 60s git submodule update --init --recursive || true
+
+# Loop through each submodule and reset it
+for SUBMODULE in "${SUBMODULES[@]}"
+do
+    echo "Resetting $SUBMODULE"
+    cd $SUBMODULE
+
+    # Resetting the submodule
+    git reset --hard
+
+    # Navigate back to the root directory
+    cd $REPO_DIR
+done
 
 # Try to run tsx
 if ! tsx .; then
     echo "tsx failed, running npm install..."
     npm install
+
     # Try to run tsx again
     tsx .
 fi
