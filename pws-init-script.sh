19e2092ae3c72e569b5e8bd9746dc94eaa688a67
Matěj Suchánek <jsem@matejsuchanek.cz>
2023-11-25 14:36:30 +0100

    cu: updating submodule and repo

--- pws-init-script.sh
+++ pws-init-script.sh
@@ -11,22 +11,24 @@ cd $REPO_DIR
 
 timeout 60s git submodule update --init --recursive || true
 
-# Loop through each submodule and reset it
-for SUBMODULE in "${SUBMODULES[@]}"
-do
-    echo "Resetting $SUBMODULE"
-    cd $SUBMODULE
-
-    # Resetting the submodule
-    git reset --hard
-
-    # Navigate back to the root directory
-    cd $REPO_DIR
-done
-
 # Try to run tsx
 if ! tsx .; then
-    echo "tsx failed, running npm install..."
+    echo "tsx failed, resetting submodules..."
+
+    # Loop through each submodule and reset it
+    for SUBMODULE in "${SUBMODULES[@]}"
+    do
+        echo "Resetting $SUBMODULE"
+        cd $SUBMODULE
+
+        # Resetting the submodule
+        git reset --hard
+
+        # Navigate back to the root directory
+        cd $REPO_DIR
+    done
+
+    echo "running npm install..."
     npm install
 
     # Try to run tsx again
